2e9cfe1124953ceaf5ed032617a3a6282f64c96e
Sydney Orr <sydneywhitters@gmail.com>
2021-12-01 09:58:49 -0700

    about

---
+++ client/src/components/Terminology.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Terminology = () => {
+  return (
+    <div>
+      <h1>Terminology</h1>
+    </div>
+  );
+};
+
+export default Terminology;
--- client/src/components/routes.js
+++ client/src/components/routes.js
@@ -1,11 +1,19 @@
 import React from "react"
+import About from "./About";
 import Home from "./Home";
+import Life from "./Life";
 import Login from "./Login";
 import NoMatch from "./NoMatch";
+import Projects from "./Projects";
 import Register from "./Register";
+import Terminology from "./Terminology";
 import Things from "./Things";
 
 export const routes = [
   { pathname: "/", title: "Home", component: Home},
-  // { pathname: "/things", title: "Things", component: Things},
+  { pathname: "/terminology", title: "Terminology & Formulas", component: Terminology},
+  { pathname: "/relatestolife", title: "Relates to life", component: Life},
+  { pathname: "/about", title: "About", component: About},
 ];
+
+// terms, what is calc, founder of calc, origin of calc, relate to real life
