303dcafcf57afbed77af3b212ad52a86294e0ce6
Sydney Orr <sydneywhitters@gmail.com>
2021-12-06 19:19:43 -0700

    done

--- client/src/components/Derivatives.js
+++ client/src/components/Derivatives.js
@@ -7,17 +7,17 @@ const Derivatives = () => {
   return (
     <div style={{color: 'white'}}>
       <h1 style={{textAlign: 'center'}}>Derivatives</h1>
-      <h2>What is a derivative?</h2>
+      <h2 style={{color: 'white'}}>What is a derivative?</h2>
       <p>Derivatives show us the instantaneous rate of change of a function at a given point. They tell us whether a function is increasing or decreasing.</p>
       <TermBox>
-        <h3>Example</h3>
+        {/* <h3>Example</h3> */}
         <p>In the old nursery rhyme, “Jack and Jill went up the hill to fetch a pail of water, Jack fell down and broke his crown…”. If we are looking at this statement, we can label the hill as the function in terms of elevation and distance. As Jack and Jill are walking up the hill, our derivative is positive due to their increase in elevation as the hill goes up. Once they reach the top, the hill is at a turning point where they are no longer gaining or losing elevation. This is because the slope of the hill at this turning point is flat. Now as Jack begins to fall, he loses elevation making our derivative negative. 
 </p>
       </TermBox>
-      <h2>Second Derivative</h2>
-      <p>Second derivatives show the instantaneous rate of change of the derivative function. They tell us how a function is increasing or decreasing  (speeding up, slowing down, or increase at the same rate).</p>
+      <h2 style={{color: 'white'}}>Second Derivative</h2>
+      <p>Second derivatives show the instantaneous rate of change of the derivative function. They tell us how a function is increasing or decreasing  (speeding up, slowing down, or increasing at the same rate).</p>
       <TermBox>
-        <h3>Example</h3>
+        {/* <h3>Example</h3> */}
         <p>In the nursery rhyme example from above, we found our derivative function. If we want to look at how the steepness of the hill changes, we need to analyze the changes in the slope. In other words, while Jack and Jill are navigating the hill, we want to know if, when,  and by how much they are gaining or losing elevation.</p>
       </TermBox>
       <Image src={DerivativePic} style={{display: 'flex', marginTop: '100px', justifyContent: 'center'}}/>
--- client/src/components/Terminology.js
+++ client/src/components/Terminology.js
@@ -21,27 +21,27 @@ const Terminology = () => {
     <h2 style={{textAlign: 'center', marginBottom: '70px', color: 'white'}}>Important terminology</h2>
     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
     <div style={{color: 'white', alignItems: 'flex-start', marginRight: '60px'}}>
-      <h3 style={{color: 'pink'}}>Differentiable</h3>
+      <h3 style={{color: 'lightblue'}}>Differentiable</h3>
       <ul><li>A function is differentiable at some point if there is a defined derivative at that point.</li></ul>
-      <h3 style={{color: 'pink'}}>Continuous</h3>
+      <h3 style={{color: 'lightblue'}}>Continuous</h3>
       <ul><li>A function is continuous if it has no breaks or gaps. The h(x) function in figure 1 models a continuous function, whereas function f(x) shows a gap where x=1 and is therefore not continuous.</li></ul>
-      <h3 style={{color: 'pink'}}>Concavity</h3>
+      <h3 style={{color: 'lightblue'}}>Concavity</h3>
       <ul><li>Concavity is directly related to derivatives and second derivatives. Concave up is the result of a derivative that starts out negative, is briefly 0, and then becomes positive. The second derivative of a concave up function is always positive. Similarly, a function that is concave down starts with a positive derivative, is briefly 0, and then is negative. The second derivative of this function will be negative.</li></ul>
-      <h3 style={{color: 'pink'}}>Domain</h3>
+      <h3 style={{color: 'lightblue'}}>Domain</h3>
       <ul><li>The domain of a function consists of all the possible values of the independent variable (x-axis).</li></ul>
-      <h3 style={{color: 'pink'}}>Limits</h3>
+      <h3 style={{color: 'lightblue'}}>Limits</h3>
       <ul><li>The limit is the value of a function as it gets closer and closer to some number in the domain. In figure 1, we can see in each function that as x approaches 1, the value gets closer and closer to 3.</li></ul>
-      <h3 style={{color: 'pink'}}>Tangent lines</h3>
+      <h3 style={{color: 'lightblue'}}>Tangent lines</h3>
       <ul><li>A tangent line is a line that touches a point on a curve. It must only touch one point and the slope of the tangent line is the slope of the function at that particular point. This is also called the instantaneous rate of change. Tangent Line examples are modeled in figure 2.</li></ul>
-      <h3 style={{color: 'pink'}}>Secant lines</h3>
+      <h3 style={{color: 'lightblue'}}>Secant lines</h3>
       <ul><li>A secant line is a line that touches two different points of the same function. This line is used to calculate the average rate of change between these two points. Secant line examples are modeled in figure 3.</li></ul>
-      <h3 style={{color: 'pink'}}>Extreme Values</h3>
+      <h3 style={{color: 'lightblue'}}>Extreme Values</h3>
       <ul><li>For some continuous interval on a function there must be a maximum and a minimum in that interval. These are called the extreme values.</li></ul>
-      <h3 style={{color: 'pink'}}>Global Optimization</h3>
+      <h3 style={{color: 'lightblue'}}>Global Optimization</h3>
       <ul><li>Global optimization is the method of finding the global minima and maxima of a function.</li></ul>
-      <h3 style={{color: 'pink'}}>Applied Optimization</h3>
+      <h3 style={{color: 'lightblue'}}>Applied Optimization</h3>
       <ul><li>Applied optimization is the method of maximizing or minimizing a quantity in a practical problem.</li></ul>
-      <h3 style={{color: 'pink'}}>Related Rates</h3>
+      <h3 style={{color: 'lightblue'}}>Related Rates</h3>
       <ul><li>When multiple quantities are related through an equation and are changing over time, we can differentiate that equation to find the related rates.
 </li></ul>
     </div>
